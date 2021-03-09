67fb9b32f997a8253b74d536f3420e8fc7e8eb6d
anja-tanovic <tanovicanja@gmail.com>
2021-03-09 11:09:08 +0100

    Treptanje sa tajmerom 0

--- Vezba2/Zadatak1/main.c
+++ Vezba2/Zadatak1/main.c
@@ -1,15 +1,34 @@
 #include <avr/io.h>
-#include <util/delay.h>
+#include <avr/interrupt.h>
+//treptanje f=2Hz
+unsigned long t0=0;
+
+ISR(TIMER0_COMPA_vect)
+{
+	t0++;
+}
+
 
 int main(void)
 {
-	//treptanje f=2Hz
+	//inicijalizacija tajmera 0:
+	TCCR0A = 0x02; // CTC mod
+	TCCR0B = 0x03; // fclk = fosc/64
+	OCR0A = 249; //perioda tajmera 0: 250 Tclk (OCR0A + 1 = 250)
+	TIMSK0 = 0x02; //dozvola prekida tajmera 0
+	//usled dostizanja vrednosti registra OCR0A
+
+	sei(); //I = 1 (dozvola prekida)
+
 	DDRB |= 1 << 5; //PB5 je izlaz za diodu
+	PORTB |= 1<<5; //setovanje diode na 1 (led on)
+
 	while(1) {
-		PORTB |= 1<<5; //setovanje diode na 1 (led on)
-		_delay_ms(500);
-		PORTB &= ~(1<<5); //resetovanje diode na 0 (led off)
-		_delay_ms(500);
+		if (t0 == 500) {
+			t0=0;
+			//PORTB = 0x00;
+			PORTB ^= 1<<5 ; //xor, invertuje stalno PB5
+		}
 	}
 	return 0;
 }
