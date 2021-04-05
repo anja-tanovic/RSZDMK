e174d5b0717028ceccc048d727ce59bcee34dd4e
anja-tanovic <tanovicanja@gmail.com>
2021-04-05 10:10:11 +0200

    prvi zadatak

---
+++ Vezba4/Zadatak0/util.c
@@ -0,0 +1,14 @@
+/**
+ * @file util.c
+ * @brief biblioteka koja definise pomocne funkcije
+ * @author Anja Tanovic
+ * @date 23-03-2021
+ * @version 1.0
+ */
+
+#include "util.h"
+
+unsigned long calculateHalfPeriod(unsigned long period)
+{
+	return (period/2);
+}
